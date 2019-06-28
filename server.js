8de732808304d259cdb6ea9723cdf67241e78802
Daniel Mage <dmage20@gmail.com>
2019-06-28 17:38:56 +0700

    init commit

---
+++ server.js
@@ -0,0 +1,13 @@
+const express = require('express')
+
+const app = express()
+
+const PORT = process.env.PORT || 3000
+
+app.listen(PORT, ()=>{
+    console.log('server listening on ' + PORT)
+})
+
+app.get('/', (req, res)=>{
+    res.send('API is running')
+})
